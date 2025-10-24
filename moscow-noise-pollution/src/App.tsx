d8e619b0590f4c831e1dc350b2e7669b06f088e5
Anna Loginova <zv1376a@gmail.com>
2025-10-24 16:52:29 +0300

    feat: update styles

--- moscow-noise-pollution/src/App.tsx
+++ moscow-noise-pollution/src/App.tsx
@@ -2,18 +2,20 @@ import IntroductionContainer from "./elements/Introduction.tsx";
 import NoisePollutionGeneralContainer from "./elements/NoisePollutionGeneral.tsx";
 import MoscowContainer from "./elements/Moscow.tsx";
 import HowToTurnDownContainer from "./elements/HowToTurnDown.tsx";
+import {clsx} from "clsx";
 
 
 const App = () => {
   return (
     <>
         <header>
-            <h1>The Sound That Never Sleeps</h1>
         </header>
-        <IntroductionContainer />
-        <NoisePollutionGeneralContainer />
-        <MoscowContainer />
-        <HowToTurnDownContainer />
+        <main className={clsx("max-w-2xl", "mx-auto", "max-md:mx-7", "my-15")}>
+            <IntroductionContainer />
+            <NoisePollutionGeneralContainer />
+            <MoscowContainer />
+            <HowToTurnDownContainer />
+        </main>
         <footer>
             <a href={""}>
                 <img src="./github-mark.png" width={40} height={40} />
