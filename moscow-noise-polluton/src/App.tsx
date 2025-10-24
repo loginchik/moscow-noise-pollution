9d29230e99521f7a4b2f1895c7af3ecb51a74ca5
Anna Loginova <zv1376a@gmail.com>
2025-10-24 14:40:39 +0300

    refactor: update styles

--- moscow-noise-polluton/src/App.tsx
+++ moscow-noise-polluton/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import IntroductionContainer from "./elements/Introduction.tsx";
 import NoisePollutionGeneralContainer from "./elements/NoisePollutionGeneral.tsx";
 import MoscowContainer from "./elements/Moscow.tsx";
@@ -8,10 +7,18 @@ import HowToTurnDownContainer from "./elements/HowToTurnDown.tsx";
 const App = () => {
   return (
     <>
+        <header>
+            <h1>The Sound That Never Sleeps</h1>
+        </header>
         <IntroductionContainer />
         <NoisePollutionGeneralContainer />
         <MoscowContainer />
         <HowToTurnDownContainer />
+        <footer>
+            <a href={""}>
+                <img src="./github-mark.png" width={40} height={40} />
+            </a>
+        </footer>
     </>
   )
 }
